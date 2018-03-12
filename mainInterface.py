6a9fdc2ad7d99069d9b8061479c5f06a9aa2faeb
lynn lefebvre <bluetechgirl@users.noreply.github.com>
2018-03-12 01:14:50 -0500

    Included copyright notice at beginning of program

--- mainInterface.py
+++ mainInterface.py
@@ -1,5 +1,22 @@
 #!/usr/bin/python3
 
+# Interface for a raspberry pi zero based smartwatch using the adafruit pitft - 2.8" touchscreen
+#
+# Copyright (C) 2018  Sarah Lynn Lefebvre
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see https://www.gnu.org/licenses/gpl.html.
+
 from tkinter import *
 import os
 
